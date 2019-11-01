52d0f9771d9d69c48165cd3fdcb341ace5ac9586
宮下翔伍 <syogo0323sekaowa@docomo.ne.jp>
2019-11-01 11:22:46 +0900

    床の作成とその他基本要素の作成

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject trans_player;
+    void Update()
+    {
+        
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.Translate(1, 0, 0);
+            }
+            else if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.Translate(-1, 0, 0);
+            }
+        trans_player.transform.position =
+            new Vector3(Mathf.Clamp(trans_player.transform.position.x, -10, 10), 4.7f, 0);
+      
+    }
+   
+}
